b50ace08511bfc5d61842737fa43032b5b9e370b
joanie <morgane.francomme@gmail.com>
2021-03-08 12:17:06 +0400

    Commencement du code HTML

---
+++ accueil.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil</title>
+</head>
+<body>
+	<header>
+		<img src="img/Logo_GBAF.png">
+	</header>
+
+	<section>
+		
+	</section>
+
+	<section>
+		
+	</section>
+
+	<footer>
+		
+	</footer>
+</body>
+</html>
